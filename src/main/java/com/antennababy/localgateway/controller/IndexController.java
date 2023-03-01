216913e170548a6f883b7ef63797173837b6b70b
lixiangyang <LIXIANGYANGE@enn.cn>
2023-03-01 10:10:32 +0800

    增加context_path

--- src/main/java/com/antennababy/localgateway/controller/IndexController.java
+++ src/main/java/com/antennababy/localgateway/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.antennababy.localgateway.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,12 @@ import java.io.IOException;
 
 @RequestMapping("/")
 @Controller
+@Slf4j
 public class IndexController {
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
     @RequestMapping
     public void index(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/static/index.html");
+        response.sendRedirect(contextPath+"/static/index.html");
     }
 }
