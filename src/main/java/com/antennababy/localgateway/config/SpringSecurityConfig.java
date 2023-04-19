241e55bb523f4be76d2b8aefa4bd1166c2a69fd8
lixiangyang <LIXIANGYANGE@enn.cn>
2023-04-19 13:46:35 +0800

    调整jvm内存占用

--- src/main/java/com/antennababy/localgateway/config/SpringSecurityConfig.java
+++ src/main/java/com/antennababy/localgateway/config/SpringSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +24,25 @@ public class SpringSecurityConfig {
     private String password;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,RememberMeServices rememberMeServices) throws Exception {
         http.authorizeHttpRequests().requestMatchers("/**").hasRole("USER").and().formLogin();
+        http.rememberMe((remember) -> remember
+                .rememberMeServices(rememberMeServices)
+        );
         return http.csrf(AbstractHttpConfigurer::disable).build();
+    }
+
+    /**
+     * 记住我功能
+     * @param userDetailsService 用户信息服务
+     * @return
+     */
+    @Bean
+    RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+        TokenBasedRememberMeServices.RememberMeTokenAlgorithm encodingAlgorithm = TokenBasedRememberMeServices.RememberMeTokenAlgorithm.SHA256;
+        TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices("springRocks", userDetailsService, encodingAlgorithm);
+        rememberMe.setMatchingAlgorithm(TokenBasedRememberMeServices.RememberMeTokenAlgorithm.MD5);
+        return rememberMe;
     }
     @Bean
     public UserDetailsService userDetailsService() {
